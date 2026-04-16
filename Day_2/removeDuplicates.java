352bf99893c1595f06938dd6bf6de4f2a107fd82
Coder-amrut <amrutkiranskshirsagar@gmail.com>
2026-04-16 23:29:36 +0530

    Day 4 added

--- Day_2/removeDuplicates.java
+++ Day_2/removeDuplicates.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 class removeDuplicates {
     public int removeDuplicates(int[] nums) {
@@ -11,3 +12,18 @@ class removeDuplicates {
       return i+1;
     }
 }
+=======
+
+class removeDuplicates {
+    public int removeDuplicates(int[] nums) {
+      if(nums.length == 0) return 0;
+      int i = 0;
+      for(int j=0; j<nums.length; j++){
+        if(nums[j] != nums[i])
+            i++;
+        nums[i] = nums[j];    
+      }
+      return i+1;
+    }
+}
+>>>>>>> af5d60a (Day 4 created)
--- Day_3/removeUserGivenElement.java
+++ Day_3/removeUserGivenElement.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class removeUserGivenElement {
     public int removeElement(int[] nums, int val) {
         int k=0;
@@ -10,3 +11,17 @@ class removeUserGivenElement {
         return k;
     }
 }
+=======
+class removeUserGivenElement {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0; i<nums.length; i++){
+            if(val != nums[i]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+>>>>>>> af5d60a (Day 4 created)
