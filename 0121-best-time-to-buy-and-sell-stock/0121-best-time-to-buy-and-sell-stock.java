397defb9a49d0ce26e40569446704743f3b23307
Amrutkiran Kshirsagar <amrutkiranskshirsagar@gmail.com>
2026-07-30 00:00:30 +0530

    Time: 2 ms (42.26%), Space: 94.3 MB (86.1%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -17,3 +17,24 @@ class Solution {
         return result;
     }
 }
+       // int smallest = prices[0];
+        // int k=0;
+        // //buying prize
+        // for(int i=1; i<prices.length; i++){
+        //     if(prices[i] < smallest){
+        //         smallest = prices[i];
+        //         k=i;
+        //     }
+            
+        // }   
+        // //selling prize
+        // int max = prices[0];
+        // for(int j=k; j<prices.length; j++){
+        //     if(prices[j] > max){
+        //         max = prices[j];
+                
+        //     }
+            
+        // }
+        // int result = max - smallest;
+        // return result;  can u do chage on my code
