1804724316e79d4a8e804cd8ffe3ac002068e12f
Amrutkiran Kshirsagar <amrutkiranskshirsagar@gmail.com>
2026-09-14 20:52:28 +0530

    Time: 1 ms (95.73%), Space: 43 MB (77.78%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,26 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        //List<Character> ab = new ArrayList<>();
         StringBuilder ans = new StringBuilder();
+
         char[] word11 = word1.toCharArray();
         char[] word22 = word2.toCharArray();
+
         int i=0;
-        while(word1.length() > i || word2.length() > i){
-            if(i<word11.length){
+        while(word1.length() > i || word2.length() > i ){
+            if(i < word11.length){
                 ans.append(word11[i]);
-                //break;
             }
-            if(i<word22.length){
+            if(i < word22.length){
                 ans.append(word22[i]);
-                //break;
             }
             i++;
         }
-        //StringBuilder ans = new StringBuilder();
-        //for(char ch : ab){
-          //  ans.append(ch);
-        //}
         return ans.toString();
-        
     }
 }
