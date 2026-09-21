4ae92f30b210dc80498ddbde088839442e69b5c0
Amrutkiran Kshirsagar <amrutkiranskshirsagar@gmail.com>
2026-09-21 16:10:55 +0530

    Time: 0 ms (100%), Space: 42.3 MB (66.23%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        boolean result = true;
+
+        while (n != 1 && n != 4) {
+
+            int sum = 0;
+
+            while (n > 0) {
+
+                int reminder = n % 10;
+                sum += reminder * reminder;
+                n = n / 10;
+            }
+
+            n = sum;
+        }
+
+        if (n == 1) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+}
