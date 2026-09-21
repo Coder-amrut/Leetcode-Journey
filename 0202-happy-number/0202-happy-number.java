72296defd7710968d6871c1987461c3775e2164b
Amrutkiran Kshirsagar <amrutkiranskshirsagar@gmail.com>
2026-09-21 16:33:44 +0530

    Time: 1 ms (77.84%), Space: 42.4 MB (50.75%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,28 +1,66 @@
 class Solution {
     public boolean isHappy(int n) {
-
-        boolean result = true;
-
-        while (n != 1 && n != 4) {
-
-            int sum = 0;
-
-            while (n > 0) {
-
-                int reminder = n % 10;
-                sum += reminder * reminder;
-                n = n / 10;
+        HashSet<Integer> ans = new HashSet<>();
+        while(n != 1){
+            if(ans.contains(n)){
+                return false;
             }
 
+            ans.add(n);
+
+            int sum = 0;
+            while( n > 0){
+                int reminder = n % 10;
+                sum += reminder*reminder;
+                n = n/10;
+            }
             n = sum;
         }
+        return true;
+        
 
-        if (n == 1) {
-            result = true;
-        } else {
-            result = false;
-        }
 
-        return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // boolean result = true;
+
+        // while (n != 1 && n != 4) {
+
+        //     int sum = 0;
+
+        //     while (n > 0) {
+
+        //         int reminder = n % 10;
+        //         sum += reminder * reminder;
+        //         n = n / 10;
+        //     }
+
+        //     n = sum;
+        // }
+
+        // if (n == 1) {
+        //     result = true;
+        // } else {
+        //     result = false;
+        // }
+
+        // return result;
+
+
+
     }
 }
