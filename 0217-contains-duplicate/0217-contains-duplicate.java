f250d572a6a8a3c32892c017cd9229647aa24a18
Amrutkiran Kshirsagar <amrutkiranskshirsagar@gmail.com>
2026-09-22 13:43:31 +0530

    Time: 17 ms (88.43%), Space: 108.2 MB (47.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,20 @@
+import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]){
+        // Arrays.sort(nums);
+        // for(int i=0; i<nums.length-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        HashSet<Integer> ab = new HashSet<>();
+        for(int num : nums){
+            if(ab.contains(num)){
                 return true;
             }
+            ab.add(num);  
         }
         return false;
     }
