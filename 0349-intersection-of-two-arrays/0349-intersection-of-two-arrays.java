09f087acc60a8f6bcf6e566dcd1c75213f158357
Amrutkiran Kshirsagar <amrutkiranskshirsagar@gmail.com>
2026-09-22 23:14:54 +0530

    Time: 2 ms (97.7%), Space: 44.8 MB (69.96%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,18 +1,37 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> ab = new ArrayList<>();
-        for(int i=0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j] && !ab.contains(nums1[i])){
-                    ab.add(nums1[i]);
-                    break;
-                }
+        // List<Integer> ab = new ArrayList<>();
+        // for(int i=0; i<nums1.length; i++){
+        //     for(int j=0; j<nums2.length; j++){
+        //         if(nums1[i] == nums2[j] && !ab.contains(nums1[i])){
+        //             ab.add(nums1[i]);
+        //             break;
+        //         }
+        //     }
+        // }
+        // int nums3[] = new int[ab.size()];
+        // for(int i=0; i<ab.size(); i++){
+        //     nums3[i] = ab.get(i);
+        // }
+        // return nums3;
+
+        HashSet<Integer> ab = new HashSet<>();
+        List<Integer> abc = new ArrayList<>();
+        for(int num : nums1){
+            ab.add(num);
+        }
+        for (int num : nums2) {
+            if (ab.contains(num)) {
+                abc.add(num);
+                ab.remove(num);
             }
+        }  
+        int[] ans = new int[abc.size()];
+
+        for (int i = 0; i < abc.size(); i++) {
+            ans[i] = abc.get(i);
         }
-        int nums3[] = new int[ab.size()];
-        for(int i=0; i<ab.size(); i++){
-            nums3[i] = ab.get(i);
-        }
-        return nums3;
+
+        return ans;
     }
-}
+}     
